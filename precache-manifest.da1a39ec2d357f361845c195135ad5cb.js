5146cbdf0a67eec846470d890238e7c052882ec5
Kasaract <garynguyen2018@gmail.com>
2020-01-26 08:03:54 -0500

    Updates

---
+++ precache-manifest.da1a39ec2d357f361845c195135ad5cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c378b0610cf9f1a9e77d9877afc2e1e9",
+    "url": "/Matchr-Proto/index.html"
+  },
+  {
+    "revision": "93b19ece7cd8f3583408",
+    "url": "/Matchr-Proto/static/css/main.55c853ec.chunk.css"
+  },
+  {
+    "revision": "c79a0e3c26aaafa14040",
+    "url": "/Matchr-Proto/static/js/2.c54986a7.chunk.js"
+  },
+  {
+    "revision": "4fbb664bfbe1cf0e394a9cfc5137f9b6",
+    "url": "/Matchr-Proto/static/js/2.c54986a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93b19ece7cd8f3583408",
+    "url": "/Matchr-Proto/static/js/main.b6b2d725.chunk.js"
+  },
+  {
+    "revision": "40e8d887bbb15848f119",
+    "url": "/Matchr-Proto/static/js/runtime-main.973ed4aa.js"
+  },
+  {
+    "revision": "62c92389b7f5cb64cdea6fd18babdc71",
+    "url": "/Matchr-Proto/static/media/headshot.62c92389.jpg"
+  },
+  {
+    "revision": "ce49a946057f44e7c3a8fd474aa993f9",
+    "url": "/Matchr-Proto/static/media/spotify.ce49a946.svg"
+  }
+]);
